045ee07d1bb3a105063b52c66ed7688a72b3ca67
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-01-18 20:41:00 -0800

    Homework Burger

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'fuchibabe13',
+    password : '',
     database : 'burgers_db'
   });
 };
