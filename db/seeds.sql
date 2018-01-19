b982792f1f8cb8be0785ec9841e056488ea2de2e
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2018-01-18 20:36:27 -0800

    Homework Burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name) VALUES ('CALI Surf Burger');
+INSERT INTO burgers(burger_name) VALUES ('BBQ Ribs Burger');
+INSERT INTO burgers(burger_name) VALUES ('Two Pounder Burger');
+
